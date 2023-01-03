b9cf015f866ed951cbf31d862c8478eb94e0cc4b
PabloIbannez <p.ibanez.fre@gmail.com>
2023-01-03 19:54:36 +0100

    Added simple test

---
+++ tests/simple.py
@@ -0,0 +1,30 @@
+import pyGrained
+import pyGrained.models
+
+resolution = 300 #Number of atoms per bead
+steps      = 10000 #Number of minimization steps
+
+pathToPDB = "./data/p22/5uu5.pdb"
+outputFile = "p22_sbcg"
+
+test = pyGrained.models.SBCG("p22_sbcg",pathToPDB)
+test.generateModel(resolution,steps)
+
+#Models to be used in topology generation. Elastic network model for the bonds and a proximity model of alpha carbons for native contacts.
+
+model = {"bondsModel":{"name":"ENM",
+                       "parameters":{"enmCut":12.0}},
+         "nativeContactsModel":{"name":"CA",
+                                "parameters":{"ncCut":8.0}}
+         }
+
+test.generateTopology(model)
+
+#Generate .json with all information
+test.generateSimulation(outputFile+".json",K=5.0,epsilon=1.0,D=1.2)
+
+#Write a PDB file with the coarse grained model
+test.writePDBcg(outputFile+".pdb")
+
+#Write a superpunto file
+test.writeSPcg(outputFile+".sp")
